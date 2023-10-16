c46518248d696ca0ce665310668c003547f5255e
budiharta <budiharta_21@live.com>
2023-10-16 11:34:21 +0800

    add more information

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -4,9 +4,19 @@ export default function TestPage() {
   const headersInstance = headers();
   const country = headersInstance.get("cf-ipcountry");
 
+  const allHeaders: any[] = [];
+  headersInstance.forEach((value, key) => {
+    allHeaders.push({ key, value });
+  });
+
   return (
     <div className="flex justify-center items-center min-h-screen">
-      Hello! {country ? country : "unknown place"}
+      <div>
+        <div className="text-center font-bold mb-10">
+          Hello! {country ? country : "unknown place"}
+        </div>
+        <div className="p-10">{JSON.stringify(allHeaders)}</div>
+      </div>
     </div>
   );
 }
