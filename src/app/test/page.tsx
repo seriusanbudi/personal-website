2e578f8690ee7eef574ffdc8322b32cc34e278d1
budiharta <budiharta_21@live.com>
2023-10-16 11:27:04 +0800

    clean up code

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -3,11 +3,6 @@ import { headers } from "next/headers";
 export default function TestPage() {
   const headersInstance = headers();
   const country = headersInstance.get("cf-ipcountry");
-  console.log({ country });
-
-  headersInstance.forEach((value, key) => {
-    console.log({ value, key });
-  });
 
   return (
     <div className="flex justify-center items-center min-h-screen">
